e305900841b1526c3e5288f78dd966f808e2eb90
liyinan1 <liyinan1@360.cn>
2022-04-05 17:31:03 +0800

    add pte printout

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+static void
+vmprint_v(pagetable_t pagetable, int level)
+{
+  for(int i = 0; i < 512; i ++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V) {
+      uint64 child = PTE2PA(pte);
+      for(int l = 2; l > level; l --) printf(".. ");
+
+      printf("..%d: pte %p pa %p\n", i, pte, child);
+
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0) vmprint_v((pagetable_t)child, level -1);
+    }
+  }
+}
+
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_v(pagetable, 2);
+}
